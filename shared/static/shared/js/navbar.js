e621061b21ad8e3d7e240368ad4f8a4f76e0147d
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2024-02-26 17:50:31 +0000

    Add responsive navbar with hamburger menu

---
+++ shared/static/shared/js/navbar.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  if (menu.style.right === "-100%") {
+    menu.style.right = "0";
+  } else {
+    menu.style.right = "-100%";
+  }
+}
