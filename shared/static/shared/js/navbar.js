4e6820ce336b63ae3865a285da955fc3e3b6cb62
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2024-02-26 21:40:31 +0000

    Update menu width and padding in main.css, and add event listener for hamburger icon in navbar.js

--- shared/static/shared/js/navbar.js
+++ shared/static/shared/js/navbar.js
@@ -1,8 +1,20 @@
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  if (menu.style.right === "-100%") {
-    menu.style.right = "0";
-  } else {
-    menu.style.right = "-100%";
-  }
-}
+// function toggleMenu() {
+//   const menu = document.getElementById("menu");
+//   if (menu.style.right === "-100%") {
+//     menu.style.right = "0";
+//   } else {
+//     menu.style.right = "-100%";
+//   }
+// }
+
+document
+  .getElementById("hamburger-icon")
+  .addEventListener("click", function () {
+    var menu = document.getElementById("menu");
+    var style = getComputedStyle(menu); // get the computed style of #menu
+    if (style.right === "0px") {
+      menu.style.right = "-100%";
+    } else {
+      menu.style.right = "0px";
+    }
+  });
