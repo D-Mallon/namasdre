12d1785b40cbeaa765dfe0b8f7968bfb626c08f3
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2024-02-28 11:11:56 +0000

    Add contact page and update URLs and views

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, contact
+from core import views
 
 urlpatterns = [
     path('', home, name='home'),
-    
+    path('contact/', contact, name='contact'),  # This should serve your contact page
 ]
