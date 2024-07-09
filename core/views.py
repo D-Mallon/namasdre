c096e14a804f66e580640c4997c5991a330a84a3
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2024-07-09 15:16:36 +0100

    feat: Add welcome message to contact page

--- core/views.py
+++ core/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
     return render(request, 'core/index.html')
 
+# def contact(request):
+#     return render(request, 'core/contact_page.html')
+
+
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        
+        # Send email
+        send_mail(
+            f"Contact Form Submission from {name}",
+            message,
+            email,
+            ['info@namasdre.com'],
+            fail_silently=False,
+        )
+        
+        return HttpResponse('Thank you for your message.')
+
     return render(request, 'core/contact_page.html')
