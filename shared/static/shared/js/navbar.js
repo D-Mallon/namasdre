aaf99e07cf70403dd42461361d2a808ce222b96d
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2024-07-14 13:00:13 +0100

    refactor: Improve hamburger menu functionality and styling

--- shared/static/shared/js/navbar.js
+++ shared/static/shared/js/navbar.js
@@ -1,15 +1,6 @@
-// function toggleMenu() {
-//   const menu = document.getElementById("menu");
-//   if (menu.style.right === "-100%") {
-//     menu.style.right = "0";
-//   } else {
-//     menu.style.right = "-100%";
-//   }
-// }
-
 document
   .getElementById("hamburger-icon")
-  .addEventListener("click", function () {
+  .addEventListener("click", function (event) {
     var menu = document.getElementById("menu");
     var style = getComputedStyle(menu); // get the computed style of #menu
     if (style.right === "0px") {
@@ -17,4 +8,30 @@ document
     } else {
       menu.style.right = "0px";
     }
+    event.stopPropagation(); // Prevent click from immediately propagating to document
   });
+
+document.addEventListener("click", function (event) {
+  var menu = document.getElementById("menu");
+  if (menu.style.right === "0px") {
+    menu.style.right = "-100%";
+  }
+});
+
+// Prevent menu close when clicking inside the menu
+document.getElementById("menu").addEventListener("click", function (event) {
+  event.stopPropagation();
+});
+
+// working basic version of the hamburger menu below
+// document
+//   .getElementById("hamburger-icon")
+//   .addEventListener("click", function () {
+//     var menu = document.getElementById("menu");
+//     var style = getComputedStyle(menu); // get the computed style of #menu
+//     if (style.right === "0px") {
+//       menu.style.right = "-100%";
+//     } else {
+//       menu.style.right = "0px";
+//     }
+//   });
