46597dc8069f19e1411e81f9d6dcb40c1ddea448
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2024-07-16 16:49:00 +0100

    Update core URLs to include authentication paths and add login.html template for user registration form

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import home, contact
 from core import views
 
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('contact/', contact, name='contact'),  # This should serve your contact page
     path('timetable/', views.timetable, name='timetable'),
+    path('', include("django.contrib.auth.urls"))
 ]
