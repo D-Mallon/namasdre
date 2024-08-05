8a2319a3749522d7069be68605cbf6a6fa60a02a
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2024-08-05 14:43:47 +0100

    feat: Update password reset email templates
    The password reset email templates have been added and updated to include a personalized message and a link to reset the password. This will provide a better user experience and improve the security of the password reset process.

--- register/urls.py
+++ register/urls.py
@@ -3,7 +3,13 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/',
+    auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',
+        subject_template_name='registration/emails/password_reset_subject.txt',
+        email_template_name='registration/emails/password_reset_email.html',
+        html_email_template_name='registration/emails/password_reset_email.html',  # Ensure HTML email is specified
+    ), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
