07766b2117be482a85322a6873de4c3fd6872ff5
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2024-08-07 16:36:52 +0100

    refactor: Add custom styling for Dre class setup
    The CSS styles for the Dre class setup have been added to improve the visual appearance and user experience. This includes styling for the centered container, greeting, table, buttons, and inline form. The styles have been added to the shared CSS file for consistency and maintainability.

--- core/views.py
+++ core/views.py
@@ -1,34 +1,24 @@
-from django.shortcuts import render
-from django.core.mail import send_mail
-from django.http import HttpResponse, JsonResponse
-from .models import YogaClass, YogaClassBooking
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import JsonResponse
 from django.utils import timezone
-import datetime
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
 from django.conf import settings
-from .forms import ContactForm, YogaClassForm
-from django.core.mail import send_mail
-from .models import YogaClass
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-
-# Create your views here.
+from .models import YogaClass, YogaClassBooking
+from .forms import YogaClassForm, ContactForm
+from django.core.mail import send_mail
 
 def home(request):
     can_manage_classes = request.user.has_perm('core.manage_classes')
     return render(request, 'core/index.html', {
         'can_manage_classes': can_manage_classes
-        })
+    })
 
 def timetable(request):
     can_manage_classes = request.user.has_perm('core.manage_classes')
     now = timezone.now()
-    # Get classes for today
     today_start = now.replace(hour=0, minute=0, second=0, microsecond=0)
-    # use below if trying to bookend filter with start and end of current day. Preference is to show all future classes though.
-    # today_end = now.replace(hour=23, minute=59, second=59, microsecond=999999)
 
     online_classes = YogaClass.objects.filter(
         class_type='online',
@@ -40,70 +30,48 @@ def timetable(request):
         start_time__gte=today_start
     ).order_by('start_time')
 
-    # Debugging lines to print the context
-    print("Current Time:", now)
-    for cls in online_classes:
-        print(f"Online Class: {cls.title}, Start Time: {cls.start_time}, End Time: {cls.end_time}")
-    for cls in in_person_classes:
-        print(f"In Person Class: {cls.title}, Start Time: {cls.start_time}, End Time: {cls.end_time}")
-
     context = {
         'online_classes': online_classes,
         'in_person_classes': in_person_classes,
         'now': now,
-    }
-    return render(request, 'core/timetable.html', context, {
         'can_manage_classes': can_manage_classes
-        })
+    }
+    return render(request, 'core/timetable.html', context)
 
 def contact(request):
     can_manage_classes = request.user.has_perm('core.manage_classes')
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            # Cleaned data from the form
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
 
-            # Construct the email subject and message
             subject = f"Contact Form Submission from {name}"
             message_body = f"Message from {name} ({email}):\n\n{message}"
 
             try:
-                # Send the email using Django's send_mail function
                 send_mail(
                     subject,
                     message_body,
-                    settings.DEFAULT_FROM_EMAIL,  # From email
-                    ['namasdreinfo@gmail.com'],    # To email
+                    settings.DEFAULT_FROM_EMAIL,
+                    ['namasdreinfo@gmail.com'],
                     fail_silently=False,
                 )
-                # Redirect to the same page with a success message
                 return redirect('contact')
             except Exception as e:
-                # Handle errors and possibly display an error message
                 print(f"An error occurred while sending the email: {e}")
-                # Optionally, set an error message in the form
                 form.add_error(None, 'An error occurred while sending the email. Please try again.')
-
     else:
         form = ContactForm()
 
-    # Render the contact page with the form
-    return render(request, 'core/contact_page.html', {'form': form}, {
-        'can_manage_classes': can_manage_classes
-        })
+    return render(request, 'core/contact_page.html', {'form': form, 'can_manage_classes': can_manage_classes})
 
 @login_required
 def booking_portal(request):
     can_manage_classes = request.user.has_perm('core.manage_classes')
     if request.user.is_authenticated:
         now = timezone.now()
-        # Removing prior classes from the booking portal so that only future classes are shown
-        # today_start = now.replace(hour=0, minute=0, second=0, microsecond=0)
-        # use below if trying to bookend filter with start and end of current day. Preference is to show all future classes though.
-        # today_end = now.replace(hour=23, minute=59, second=59, microsecond=999999)
 
         online_classes = YogaClass.objects.filter(
             class_type='online',
@@ -119,16 +87,11 @@ def booking_portal(request):
             'online_classes': online_classes,
             'in_person_classes': in_person_classes,
             'now': now,
+            'can_manage_classes': can_manage_classes
         }
-        return render(request, 'core/booking_portal.html', context, {
-        'can_manage_classes': can_manage_classes
-        })
-        # return render(request, 'core/booking_portal.html')
+        return render(request, 'core/booking_portal.html', context)
     else: 
-        print('You must be logged in to access this page.')
-        return render(request, 'core/index.html', {'error': 'You must be logged in to access this page.'}, {
-        'can_manage_classes': can_manage_classes
-        })
+        return render(request, 'core/index.html', {'error': 'You must be logged in to access this page.', 'can_manage_classes': can_manage_classes})
 
 @login_required
 def add_class_to_profile(request, class_id):
@@ -149,15 +112,6 @@ def remove_class_from_profile(request, class_id):
             return JsonResponse({"success": False, "message": str(e)})
     return JsonResponse({"success": False, "message": "Invalid request method."})
 
-
-
-
-
-# @login_required  
-# def profile(request):
-#     booked_classes = YogaClassBooking.objects.filter(user=request.user).order_by('yoga_class__start_time')
-#     return render(request, 'core/profile.html', {'booked_classes': booked_classes})
-
 @login_required  
 def profile(request):
     can_manage_classes = request.user.has_perm('core.manage_classes')
@@ -171,11 +125,10 @@ def profile(request):
     context = {
         'upcoming_classes': upcoming_classes,
         'past_classes': past_classes,
+        'can_manage_classes': can_manage_classes
     }
     
-    return render(request, 'core/profile.html', context, {
-        'can_manage_classes': can_manage_classes
-        })
+    return render(request, 'core/profile.html', context)
 
 @login_required
 def manage_classes(request):
@@ -194,9 +147,20 @@ def manage_classes(request):
     else:
         form = YogaClassForm()
 
-    # Get all classes to display
-    classes = YogaClass.objects.all().order_by('start_time')
-    return render(request, 'core/manage_classes.html', {'form': form, 'classes': classes})
+    now = timezone.now()
+
+    # Separate upcoming and past classes
+    upcoming_classes = YogaClass.objects.filter(start_time__gte=now).order_by('-start_time')
+    past_classes = YogaClass.objects.filter(start_time__lt=now).order_by('-start_time')
+
+    # Pass classes to the template
+    context = {
+        'form': form,
+        'upcoming_classes': upcoming_classes,
+        'past_classes': past_classes,
+        'can_manage_classes': True,
+    }
+    return render(request, 'core/manage_classes.html', context)
 
 @login_required
 def edit_class(request, class_id):
@@ -220,33 +184,5 @@ def delete_class(request, class_id):
         return redirect('manage_classes')
     return render(request, 'core/delete_class.html', {'yoga_class': yoga_class})
 
-@login_required
-def manage_classes(request):
-    # Check if the user has the 'add_yogaclass' permission
-    if not request.user.has_perm('core.add_yogaclass'):
-        raise PermissionDenied
-
-    if request.method == 'POST':
-        form = YogaClassForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Class has been added successfully!')
-            return redirect('manage_classes')  # Redirect to the same page
-        else:
-            messages.error(request, 'There was an error adding the class. Please check the form.')
-    else:
-        form = YogaClassForm()
-
-    # Get all classes to display
-    classes = YogaClass.objects.all().order_by('start_time')
-    return render(request, 'core/manage_classes.html', {'form': form, 'classes': classes, 'can_manage_classes': True})
-
-@login_required
-def some_view(request):
-    can_manage_classes = request.user.has_perm('core.manage_classes')
-    return render(request, 'your_template.html', {
-        'can_manage_classes': can_manage_classes,
-    })
-    
 def permission_denied(request, exception):
     return render(request, 'core/permission_denied.html', status=403)
